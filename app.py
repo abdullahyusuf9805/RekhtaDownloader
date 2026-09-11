9c1ab84fac0f56bbf59e0120476d36f59f4327e1
abdullahyusuf9805 <abdullahyusuf9805@gmail.com>
2026-09-11 16:20:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,17 @@ from selenium.webdriver.support import expected_conditions as EC
 st.set_page_config(page_title="Rekhta PDF Extractor", page_icon="📚")
 st.title("📚 Rekhta PDF Extractor")
 
+st.markdown(
+    """
+    <style>
+    div[data-testid="InputInstructions"] {
+        display: none !important;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 def get_driver():
     options = webdriver.ChromeOptions()
     options.add_argument("--headless=new") 
